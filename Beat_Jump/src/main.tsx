88c63350a2bea5592c76c1e4cda3bd2250f26b06
pbc1001 <mintcho0328@gmail.com>
2026-07-21 11:50:13 +0900

    feat: 프론트엔드 인증 API 연동

--- Beat_Jump/src/main.tsx
+++ Beat_Jump/src/main.tsx
@@ -5,13 +5,16 @@ import { ThemeProvider } from '@emotion/react';
 import { theme } from './theme';
 import { GlobalStyle } from './GlobalStyle.tsx';
 import App from './App';
+import { AuthProvider } from './auth/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <BrowserRouter>
-        <App />
+        <AuthProvider>
+          <App />
+        </AuthProvider>
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
