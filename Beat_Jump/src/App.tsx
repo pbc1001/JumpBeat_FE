eca6239480997b3cc98716a32eaa35686426f67d
pbc1001 <mintcho0328@gmail.com>
2026-07-21 11:50:26 +0900

    Revert "feat: 프론트엔드 인증 API 연동"
    This reverts commit 88c63350a2bea5592c76c1e4cda3bd2250f26b06.

--- Beat_Jump/src/App.tsx
+++ Beat_Jump/src/App.tsx
@@ -8,26 +8,21 @@ import SongRegisterPage from './pages/SongResgisterPage';
 import LoadingPage from './pages/LoadingPage';
 import GameResultPage from './pages/GameResultPage';
 import TypingPracticePage from './pages/TypingPracticePage';
-import { GuestRoute, ProtectedRoute } from './auth/RouteGuards';
 
 
 
 export default function App() {
   return (
     <Routes>
-      <Route element={<GuestRoute />}>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-      </Route>
-      <Route element={<ProtectedRoute />}>
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/selectsong" element={<SongSelectPage />} />
-        <Route path="/syncedit" element={<SyncEditorPage />} />
-        <Route path="/registersong" element={<SongRegisterPage />} />
-        <Route path="/loading" element={<LoadingPage />} />
-        <Route path="/result" element={<GameResultPage />} />
-        <Route path="/typing" element={<TypingPracticePage />} />
-      </Route>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage/>}/>
+      <Route path='/main' element={<MainPage/>}/>
+      <Route path='/selectsong' element={<SongSelectPage/>}/>
+      <Route path='/syncedit' element={<SyncEditorPage/>}/>
+      <Route path='/registersong' element={<SongRegisterPage/>}/>
+      <Route path='/loading' element={<LoadingPage/>}/>
+      <Route path='/result' element={<GameResultPage/>}/>
+      <Route path='/typing' element={<TypingPracticePage/>}/>
     </Routes>
   );
 }
--- Beat_Jump/src/main.tsx
+++ Beat_Jump/src/main.tsx
@@ -5,16 +5,13 @@ import { ThemeProvider } from '@emotion/react';
 import { theme } from './theme';
 import { GlobalStyle } from './GlobalStyle.tsx';
 import App from './App';
-import { AuthProvider } from './auth/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <BrowserRouter>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
+        <App />
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
